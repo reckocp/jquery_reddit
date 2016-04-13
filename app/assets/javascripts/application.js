413dadaece88b0d6d174d3fb5cef1488307162e1
COLIN <reckocp@gmail.com>
2016-04-13 15:16:11 -0500

    Explorer Mode completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,11 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+/* globals $ */
+// This is a manifest file that"ll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or any plugin"s vendor/assets/javascripts directory can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// It"s not advisable to add code directly here, but if you do, it"ll appear at the bottom of the
 // compiled file.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
@@ -14,3 +15,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$("#highlighter").on("click", function () {
+       $("#links-children li:first").toggleClass("change-color");
+});
